a0d061d480148f016d193fc1aea349b9e9ae17e1
root <root@test.rev.poneytelecom.eu>
2016-12-11 20:45:49 +0100

    ajout variable rutorrent

--- inc/variables.sh
+++ inc/variables.sh
@@ -5,3 +5,5 @@ script="/tmp/seedbox-workflow"
 variables="/tmp/seedbox-workflow/inc/variables.sh"
 exescript="/tmp/seedbox-workflow/seedbox-workflow.sh"
 NGINXENABLE="/etc/nginx/sites-enabled"
+RUTORRENT="/var/www/rutorrent"
+
