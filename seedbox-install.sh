1bfd42a00fb6d9e28b67a461d5eccfd994fe195e
luniun <luniun@icloud.com>
2016-12-11 04:35:55 +0100

    Create seedbox-install.sh

---
+++ seedbox-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# includes
+INCLUDES="inc"
+# shellcheck source=/dev/null
+. "$INCLUDES"/variables.sh
+
+
+echo "Installation de Rutorrent-bonobox"
+echo ""
+echo "Ne pas redémmarer a la fin de l'installation"
+echo ""
+read -p "Appuyer sur une touche pour continuer ..."
+# Mise a jour
+apt-get update && apt-get upgrade -y
+
+# installation de git
+apt-get install git-core -y
+
+# Clone de rutorrent-bonobox
+cd /tmp
+git clone https://github.com/exrat/rutorrent-bonobox
+cd rutorrent-bonobox
+
+# Installation de rutorrent-bonobox
+chmod a+x bonobox.sh && ./bonobox.sh
+read -p "Appuyer sur une touche pour continuer ..."
+
+# Retour au Menu principal
+/"$script"/seedbox-workflow.sh
