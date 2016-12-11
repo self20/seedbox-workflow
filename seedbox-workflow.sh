fc52e68bd8e5f1e1e4a221c54967c8768d6bb87a
luniun <luniun@icloud.com>
2016-12-11 03:48:52 +0100

    Create seedbox-workflow.sh

---
+++ seedbox-workflow.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Script d'installation d'une seedbox workflow.
+#
+# Contenue: 
+#
+# Bonobo-box "https://github.com/exrat/rutorrent-bonobox"
+# Sickrage "https://github.com/SickRage/SickRage"
+# Couchpotato "https://github.com/CouchPotato/CouchPotatoServer"
+# Plex "https://www.plex.tv/fr/"
+# Filebot "http://www.filebot.net/"
+# H5ai "https://larsjung.de/h5ai/"
+#
+# Multi utilisateurs.
+# Installation pas a pas ou complete.
+#
+# Auteur: Luniun
+# Je ne suis l'auteur que de ce script.
+# Version: 1.0-alpha
+#
+# Prérequis:
+#
+# Un serveur dedier sous debian 8 fraichement installer.
+# systmd et ca-certificates
+# apt-get install systmd systemd-sysv ca-certificates -y
+# Redémarrage neccesaire.
+
+
+# includes
+INCLUDES="inc"
+# shellcheck source=/dev/null
+. "$INCLUDES"/variables.sh
+
+# rendre executable les scripts
+chmod -R +x /tmp/"$script"/*.sh
+clear
+
+# Introduction
+echo "### Merci d'utiliser seedbox-workflow"
+echo ""
+echo '## Ce script permet d'installer :'
+echo "                            rutorrent-bonobox (https://github.com/exrat/rutorrent-bonobox)"
+echo "                            Sickrage          (https://github.com/SickRage/SickRage)"
+echo "                            Couchpotato       (https://github.com/sarakha63/persomov)"
+echo "                            Plex Media Server (https://www.plex.tv/fr)"
+echo "                            Filebot           (http://www.filebot.net)"
+echo "                            TardiStart        (https://www.pastoutdesuite.fr/tardistart)"
+echo ""
+echo ""
+read -p "Appuyer sur une touche pour continuer ..."
