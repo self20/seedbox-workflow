4bb411022dc86d51c160230c3a58d48cedb8ca57
luniun <luniun@icloud.com>
2016-12-11 04:08:19 +0100

    Update seedbox-workflow.sh

--- seedbox-workflow.sh
+++ seedbox-workflow.sh
@@ -59,3 +59,55 @@ echo "H5ai              (https://larsjung.de/h5ai/)
 echo ""
 echo ""
 read -p "Appuyer sur une touche pour continuer ..."
+clear
+
+# Menu
+echo "Menu principal: "
+echo ""
+echo ""
+echo "1.  installation de Rutorrent-bonobox"
+echo "2.  installation de Sickrage"
+echo "3.  installation de Filebot"
+echo "4.  Installation de Couchpotato"
+echo " "
+echo "5.  Installation Config Nginx"
+echo "6.  Installation de Cerbot"
+echo ""
+echo "7.  Installation Complete"
+echo " "
+echo "9.  Reboot"
+echo "0.  Exit"
+echo ""
+echo ""
+read -p "Entrer votre choix: " menu
+if [ $menu = "1" ]
+then
+        "$script"/seedbox-install.sh
+elif [ $menu = "2" ]
+then
+        "$script"/sickrage-install.sh
+elif [ $menu = "3" ]
+then
+        "$script"/filebot-install.sh
+elif [ $menu = "4" ]
+then
+        "$script"/couchpotato-install.sh
+elif [ $menu = "5" ]
+then
+        "$script"/nginx-conf.sh
+elif [ $menu = "6" ]
+then
+        "$script"/certbot.sh
+elif [ $menu = "7" ]
+then
+        "$script"/full-install.sh
+elif [ $menu = "9" ]
+then
+        reboot
+elif [ $menu = "0" ]
+then
+        clear echo "Bye"
+else
+        echo "Bye Bye"
+fi
+exit
