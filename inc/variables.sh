b0394d2b8d29813f93fa45d986743439d7dc8f89
luniun <luniun@icloud.com>
2016-12-18 22:06:35 +0100

    Update variables.sh

--- inc/variables.sh
+++ inc/variables.sh
@@ -4,6 +4,3 @@
 script="/tmp/seedbox-workflow"
 variables="/tmp/seedbox-workflow/inc/variables.sh"
 exescript="/tmp/seedbox-workflow/seedbox-workflow.sh"
-NGINXENABLE="/etc/nginx/sites-enabled"
-RUTORRENT="/var/www/rutorrent"
-
