c798a9fdb9174240fa6d49d219940b60a6d48f64
luniun <luniun@icloud.com>
2016-12-18 22:25:16 +0100

    Delete nginx-conf.sh

--- scripts/nginx-conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-script="/tmp/seedbox-workflow"
-service nginx stop
-rm /etc/nginx/sites-enabled/rutorrent.conf
-cp "$script"/datas/conf-nginx /etc/nginx/sites-enabled/rutorrent.conf
-service nginx start
-cd $script
-./seedbox-workflow.sh
