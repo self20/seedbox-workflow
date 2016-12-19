0957cb8ec2302e08e5e19931e860abd881bcbc88
luniun <luniun@icloud.com>
2016-12-20 00:49:55 +0100

    Create couchpotato-install.sh

---
+++ scripts/couchpotato-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# includes
+script="/tmp/seedbox-workflow"
+"$script"/inc/variables.sh
+
+# installation dependances
+apt-get install git-core python python-cheetah -y
+
+# clone du repo
+git clone https://github.com/CouchPotato/CouchPotatoServer.git /opt/couchpotato
+
+# atribution des droits
+chown -R "$usersickrage":"$usersickrage" /opt/couchpotato
+
+# configuration demmarage
+cp /opt/couchpotato/init/ubuntu /etc/init.d/couchpotato
+echo -e "CP_HOME=/opt/couchpotato\nCP_USER=$usersickrage" >> /etc/default/couchpotato
+chown "$usersickrage":"$usersickrage" /etc/default/couchpotato
+chmod +x /etc/init.d/couchpotato
+update-rc.d couchpotato defaults
+service couchpotato start
+
+cd "$script"
+./seedbox-workflow.sh
