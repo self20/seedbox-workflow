b908daf19cd5c03aa4b14fc1cdc42fd06d68c031
luniun <luniun@icloud.com>
2016-12-20 01:24:25 +0100

    Update couchpotato-install.sh

--- scripts/couchpotato-install.sh
+++ scripts/couchpotato-install.sh
@@ -25,5 +25,10 @@ chmod +x /etc/init.d/couchpotato
 update-rc.d couchpotato defaults
 service couchpotato start
 
+# proxy nginx
+service couchpotato stop
+sed -i '/fin config sickrage/ a \\t## début config couchpotato ##\nlocation /couchpotato {\nproxy_pass http://127.0.0.1:5050;\nproxy_set_header Host $host;\nproxy_set_header X-Real-IP $remote_addr;\nproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n}\n\t## fin config couchpotato ##' /etc/nginx/sites-enabled/rutorrent.conf
+service couchpotato start
+
 cd "$script"
 ./seedbox-workflow.sh
